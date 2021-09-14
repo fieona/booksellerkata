9071e744c1d744bee3f5d29fd601e5ff288ffeda
fieona <muys.sofie@gmail.com>
2021-09-14 13:37:47 +0200

    ♻ Kata 1

--- src/main/java/be/xplorewood/StockList.java
+++ src/main/java/be/xplorewood/StockList.java
@@ -1,11 +1,70 @@
 package be.xplorewood;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class StockList {
 
 	// 1st parameter is the stocklist (L in example),
-	// 2nd parameter is list of categories (M in example)
-	public static String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
+	// 2nd parameter is list of requestCategories (M in example)
+	public static String stockSummary(String[] bookInventoryByCode, String[] requestCategories) {
 		// your code here
-		return null;
+		if (bookInventoryByCode.length == 0) {
+			return "";
+		}
+
+		Map<BookCategory, Integer> bookCountPerCategory = Arrays.stream(requestCategories)
+				.collect(Collectors.toMap(BookCategory::of, s -> 0));
+
+		for (String bookInventoryItem : bookInventoryByCode) {
+			BookCategory category = BookCategory.of(bookInventoryItem.charAt(0));
+			bookCountPerCategory.computeIfPresent(category, (character, currentQuantity) -> {
+				String[] codeAndQuantity = bookInventoryItem.split(" ");
+				Integer quantity = Integer.valueOf(codeAndQuantity[1]);
+				return currentQuantity + quantity;
+			});
+		}
+
+		return bookCountPerCategory.entrySet().stream()
+				.map(entry -> String.format("(%s : %s)", entry.getKey().getCategory(), entry.getValue()))
+				.collect(Collectors.joining(" - "));
+	}
+}
+
+class BookCategory {
+	private char category;
+
+	private BookCategory(char category) {
+		this.category = category;
+	}
+
+	public static BookCategory of(String categoryAsString) {
+		if (categoryAsString.length() != 1) {
+			throw new RuntimeException("Fubar");
+		}
+		return new BookCategory(categoryAsString.charAt(0));
+	}
+
+	public static BookCategory of(char categoryChar) {
+		return new BookCategory(categoryChar);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		BookCategory that = (BookCategory) o;
+		return category == that.category;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(category);
+	}
+
+	public char getCategory() {
+		return category;
 	}
 }
--- src/test/java/be/xplorewood/StockListTest.java
+++ src/test/java/be/xplorewood/StockListTest.java
@@ -3,6 +3,7 @@ package be.xplorewood;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class StockListTest {
     @Test
@@ -11,5 +12,29 @@ class StockListTest {
         String[] cd = new String[]{"A", "B"};
 
         assertThat(StockList.stockSummary(art, cd)).isEqualTo("(A : 200) - (B : 1140)");
+    }
+
+    @Test
+    void test_whenCategoriesAreEmpty_anEmptyStringIsReturned() {
+        String[] bookInventoryByCode = new String[]{"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"};
+        String[] categories = new String[]{};
+
+        assertThat(StockList.stockSummary(bookInventoryByCode, categories)).isEmpty();
+    }
+
+    @Test
+    void test_whenBookInventoryIsEmpty_anEmptyStringIsReturned() {
+        String[] bookInventoryByCode = new String[]{};
+        String[] categories = new String[]{"A", "B"};
+
+        assertThat(StockList.stockSummary(bookInventoryByCode, categories)).isEmpty();
+    }
+
+    @Test
+    void test_whenCategoryHasMultipleChars_AnErrorIsThrown() {
+        String[] bookInventoryByCode = new String[]{"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"};
+        String[] categories = new String[]{"ABC", "B"};
+
+        assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> StockList.stockSummary(bookInventoryByCode, categories)).withMessage("Fubar");
     }
 }
